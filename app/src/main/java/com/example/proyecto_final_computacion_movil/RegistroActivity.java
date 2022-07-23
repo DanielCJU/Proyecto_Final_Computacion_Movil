f1f1694ca5d10b8faf98c09241819f266cd93e66
Eduardo Espinoza <82550153+Edoespinozaa@users.noreply.github.com>
2022-07-22 21:47:05 -0400

    Agregado de RegistroActivity

--- app/src/main/java/com/example/proyecto_final_computacion_movil/RegistroActivity.java
+++ app/src/main/java/com/example/proyecto_final_computacion_movil/RegistroActivity.java
@@ -2,7 +2,10 @@ package com.example.proyecto_final_computacion_movil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        TextView btn=findViewById(R.id.txtRegistrar);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegistroActivity.class));
+            }
+        });
     }
 }
