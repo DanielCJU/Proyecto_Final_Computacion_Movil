5d1fdad84dd415718ddff891440b853703e21f1e
DanielCJU <51923424+DanielCJU@users.noreply.github.com>
2022-07-22 20:12:30 -0400

    Proyecto inicial

---
+++ app/src/main/java/com/example/proyecto_final_computacion_movil/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.proyecto_final_computacion_movil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.proyecto_final_computacion_movil.DB.DBUserAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Start = findViewById(R.id.startBtn);
+        Button Add = findViewById(R.id.addBtn);
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = "admin";
+                String password = "123";
+                try{
+                    DBUserAdapter dbUser = new DBUserAdapter(MainActivity.this);
+                    dbUser.open();
+                    if(dbUser.Login(username, password)){
+                        Toast.makeText(MainActivity.this,"Admin already created", Toast.LENGTH_LONG).show();
+                    } else {
+                        dbUser.AddUser(username,password);
+                        Toast.makeText(MainActivity.this,"Admin created", Toast.LENGTH_LONG).show();
+                    }
+                    dbUser.close();
+                }catch(Exception e)
+                {
+                    Toast.makeText(MainActivity.this,e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
